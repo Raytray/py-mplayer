88d81137b56d6ad2d7439f4db419eb3915477341
Raymond Tang <raymond@tang.me>
2013-04-24 21:07:09 -0400

    More appropriately added **kwargs

--- py-mplayer.py
+++ py-mplayer.py
@@ -20,48 +20,43 @@ def verify(filename):
     except IOError:
         return False
 
-def local(filename, ss=None):
+def local(filename, **kwargs):
     if verify(filename):
-        if ss is not None:
-            play(filename, ss=ss)
-        else:
-            play(filename)
+        play(filename, **kwargs)
     else:
-        print "Please provide a valid file to play."        
+        print "Please provide a valid file to play."
 
-def youtube(url, ss=None):
+def youtube(url, **kwargs):
     os.system('youtube-dl -o "temp-output-video.%(ext)s" ' + url)
-    if ss is not None:
-        play("temp-output-video.*", ss=ss)
-    else:
-        play("temp-output-video.*")
+    play("temp-output-video.*", **kwargs)
     os.system('rm temp-output-video.*')
+
+def get_args(args):
+    return_arguments = {}
+    if args.seek is not None:
+        return_arguments['ss'] = args.seek
+    if args.osd is not None:
+        return_arguments['osdlevel'] = args.osd
+    return return_arguments
 
 def main():
     cli_parser = argparse.ArgumentParser()
     cli_parser.add_argument("--youtube", help="url to youtube video", type=str)
     cli_parser.add_argument("--filename", help="Path to local media file to be played. Cannot use with youtube flag.", type=str)
     cli_parser.add_argument("--seek", help="Specific time to play video. Format in seconds, or h:m:s", type=str)
+    cli_parser.add_argument("--osd", help="Choose to show the on screen display for time. (0, 1, 2)", type=str)
 
     args = cli_parser.parse_args()
 
     filename = "temp-output-video.*"
 
+    kwargs = get_args(args)
+
     if args.youtube is not None:
-        if args.seek is not None:
-            youtube(args.youtube, ss=args.seek)
-        else:
-            youtube(args.youtube)
+        youtube(args.youtube, **kwargs)
 
     elif args.filename is not None:
-        if args.seek is not None:
-            local(args.filename, ss=args.seek)
-        else:
-            local(args.filename)
-
-    else:
-        print "Please utilize the --filename argument to play a local file, or the --youtube argument to play a youtube video."
-        print "Use -h for more help"
+        local(args.filename, **kwargs)
 
 if __name__ == "__main__":
     main()
